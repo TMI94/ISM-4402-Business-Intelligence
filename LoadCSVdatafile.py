70b6bd01a9ceaa6fccedc5c462326d4a0a437d49
TMI94 <70179332+TMI94@users.noreply.github.com>
2020-09-07 02:49:22 -0400

    Add files via upload

---
+++ LoadCSVdatafile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+Location = "all_040_in_12.P12.csv"
+df = pd.read_csv(Location)
+
+
+# In[2]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
---
+++ LoadEXCELdatafile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+Location = "EMS01.xls"
+df = pd.read_excel(Location)
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
